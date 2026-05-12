5a1452151c5f75cb272cecd43e852fc37a9b8357
vxtls <187420201+vxtls@users.noreply.github.com>
2026-05-12 15:26:48 -0400

    build(group5): record 2.10 reflect api transition

--- buildit.sh
+++ buildit.sh
@@ -179,6 +179,7 @@ GROUP5=(
   v2.10.0-M1+9b03e88-bootstrap
   v2.10.0-M1+bb23d76-bootstrap
   v2.10.0-M2-bootstrap
+  v2.10.0-M2+382a16e-bootstrap
   v2.10.0-M2+e9a70f5-bootstrap
   v2.10.0-M2+b4cfd11-bootstrap
   v2.10.0-M2+1adae9b-bootstrap
@@ -238,6 +239,7 @@ declare -A GROUP5_PREV=(
   [v2.10.0-M1+9b03e88-bootstrap]=v2.10.0-M1+99844e-bootstrap
   [v2.10.0-M1+bb23d76-bootstrap]=v2.10.0-M1+9b03e88-bootstrap
   [v2.10.0-M2-bootstrap]=v2.10.0-M1+bb23d76-bootstrap
+  [v2.10.0-M2+382a16e-bootstrap]=v2.10.0-M2-bootstrap
   [v2.10.0-M2+e9a70f5-bootstrap]=v2.10.0-M2-bootstrap
   [v2.10.0-M2+b4cfd11-bootstrap]=v2.10.0-M2+e9a70f5-bootstrap
   [v2.10.0-M2+1adae9b-bootstrap]=v2.10.0-M2+b4cfd11-bootstrap
@@ -253,6 +255,7 @@ declare -A GROUP5_MODE=(
   [v2.10.0-M1+99844e-bootstrap]=build
   [v2.10.0-M1+9b03e88-bootstrap]=build
   [v2.10.0-M1+bb23d76-bootstrap]=build
+  [v2.10.0-M2+382a16e-bootstrap]=build
   [v2.10.0-M2+e9a70f5-bootstrap]=build
   [v2.10.0-M2+b4cfd11-bootstrap]=build
   [v2.10.0-M2+1adae9b-bootstrap]=build
