23c71af6ab53973e4a8dc6ae5a1008fd30adb0ba
vxtls <187420201+vxtls@users.noreply.github.com>
2026-05-13 22:06:08 -0400

    fix(build): record e497667 and 6e7382b stages

--- buildit.sh
+++ buildit.sh
@@ -189,6 +189,8 @@ GROUP5=(
   v2.10.0-M2+814cf34-bootstrap
   v2.10.0-M2+814cf34+46d0d73-bootstrap
   v2.10.0-M2+46d0d73-bootstrap
+  v2.10.0-M2+e497667-bootstrap
+  v2.10.0-M2+6e7382b-bootstrap
   v2.10.0-M2+db3056f-bootstrap
   v2.10.0-M2+f7a0558-bootstrap
   v2.10.0-M2+e9a70f5-bootstrap
@@ -258,6 +260,8 @@ declare -A GROUP5_PREV=(
   [v2.10.0-M2+814cf34-bootstrap]=v2.10.0-M2+f9e8a8c+814cf34-bootstrap
   [v2.10.0-M2+814cf34+46d0d73-bootstrap]=v2.10.0-M2+814cf34-bootstrap
   [v2.10.0-M2+46d0d73-bootstrap]=v2.10.0-M2+814cf34+46d0d73-bootstrap
+  [v2.10.0-M2+e497667-bootstrap]=v2.10.0-M2+46d0d73-bootstrap
+  [v2.10.0-M2+6e7382b-bootstrap]=v2.10.0-M2+e497667-bootstrap
   [v2.10.0-M2+db3056f-bootstrap]=v2.10.0-M2+f9e8a8c-bootstrap
   [v2.10.0-M2+f7a0558-bootstrap]=v2.10.0-M2+acb2c85-bootstrap
   [v2.10.0-M2+e9a70f5-bootstrap]=v2.10.0-M2-bootstrap
@@ -283,6 +287,8 @@ declare -A GROUP5_MODE=(
   [v2.10.0-M2+814cf34-bootstrap]=build
   [v2.10.0-M2+814cf34+46d0d73-bootstrap]=build
   [v2.10.0-M2+46d0d73-bootstrap]=build
+  [v2.10.0-M2+e497667-bootstrap]=build
+  [v2.10.0-M2+6e7382b-bootstrap]=build
   [v2.10.0-M2+db3056f-bootstrap]=build
   [v2.10.0-M2+f7a0558-bootstrap]=build
   [v2.10.0-M2+e9a70f5-bootstrap]=build
