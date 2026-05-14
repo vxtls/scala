3475ec73f37c6c93b5c7f7124d128ca6368c2b87
vxtls <187420201+vxtls@users.noreply.github.com>
2026-05-14 18:51:59 -0400

    fix(bootstrap): avoid private starr ant argfile tag gap

--- src/compiler/scala/tools/ant/ScalacShared.scala
+++ src/compiler/scala/tools/ant/ScalacShared.scala
@@ -18,7 +18,8 @@ trait ScalacShared extends ScalaMatchingTask {
   def execWithArgFiles(java: Java, paths: List[String]) = {
     paths foreach (p => java.createArg() setValue ("@"+ p))
 
-    val debugString = paths map (x => " (@ = '%s')".format(io.File(x).slurp())) mkString ""
+    var debugString = ""
+    paths foreach (x => debugString += " (@ = '" + io.File(x).slurp() + "')")
     log(java.getCommandLine.getCommandline.mkString("", " ", debugString), Project.MSG_VERBOSE)
     java.executeJava()
   }
