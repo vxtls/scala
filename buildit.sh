41ccb094686dd0cb1cb1b08ba8cbc1c9d97885eb
vxtls <187420201+vxtls@users.noreply.github.com>
2026-05-14 03:29:53 -0400

    fix(build): record a786781 stage

--- buildit.sh
+++ buildit.sh
@@ -227,6 +227,7 @@ GROUP5=(
   v2.10.0-M3+bc5f42f+5acac4d-bootstrap
   v2.10.0-M3+5acac4d-bootstrap
   v2.10.0-M4-bootstrap
+  v2.10.0-M4+a786781-bootstrap
 )
 
 declare -A GROUP4_PREV=(
@@ -326,6 +327,7 @@ declare -A GROUP5_PREV=(
   [v2.10.0-M3+bc5f42f+5acac4d-bootstrap]=v2.10.0-M3+bc5f42f-bootstrap
   [v2.10.0-M3+5acac4d-bootstrap]=v2.10.0-M3+bc5f42f+5acac4d-bootstrap
   [v2.10.0-M4-bootstrap]=v2.10.0-M3+5acac4d-bootstrap
+  [v2.10.0-M4+a786781-bootstrap]=v2.10.0-M4-bootstrap
 )
 
 declare -A GROUP5_MODE=(
@@ -381,6 +383,7 @@ declare -A GROUP5_MODE=(
   [v2.10.0-M3+bc5f42f+5acac4d-bootstrap]=build
   [v2.10.0-M3+5acac4d-bootstrap]=build
   [v2.10.0-M4-bootstrap]=build
+  [v2.10.0-M4+a786781-bootstrap]=build
 )
 
 EXPECTED_ERROR2=(
