db7c0bd1117cf29a32aaeda9c41fa4fbfdcdc8df
vxtls <187420201+vxtls@users.noreply.github.com>
2026-05-14 01:55:35 -0400

    fix(build): record 3896a4 stage

--- build-scala.sh
+++ build-scala.sh
@@ -136,7 +136,8 @@ needs_previous_forkjoin_jar() {
     || [[ "$version_number" == "v2.10.0-M3+252a448-bootstrap" ]] \
     || [[ "$version_number" == "v2.10.0-M3+bdff66e-bootstrap" ]] \
     || [[ "$version_number" == "v2.10.0-M3+ce67870-bootstrap" ]] \
-    || [[ "$version_number" == "v2.10.0-M3+6355d1-bootstrap" ]]
+    || [[ "$version_number" == "v2.10.0-M3+6355d1-bootstrap" ]] \
+    || [[ "$version_number" == "v2.10.0-M3+3896a4-bootstrap" ]]
 }
 
 build_anyval_class_transition() {
