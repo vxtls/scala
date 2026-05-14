cf212b7b9f3845ee52ba02f477fe0bd04a7275fb
vxtls <187420201+vxtls@users.noreply.github.com>
2026-05-14 00:36:44 -0400

    fix(build): record db1b777 stage

--- buildit.sh
+++ buildit.sh
@@ -209,6 +209,7 @@ GROUP5=(
   v2.10.0-M2+d4b8d8d-bootstrap
   v2.10.0-M2+fe1110f-bootstrap
   v2.10.0-M3-bootstrap
+  v2.10.0-M3+db1b777-bootstrap
 )
 
 declare -A GROUP4_PREV=(
@@ -290,6 +291,7 @@ declare -A GROUP5_PREV=(
   [v2.10.0-M2+d4b8d8d-bootstrap]=v2.10.0-M2+f54e5c8-bootstrap
   [v2.10.0-M2+fe1110f-bootstrap]=v2.10.0-M2+d4b8d8d-bootstrap
   [v2.10.0-M3-bootstrap]=v2.10.0-M2+fe1110f-bootstrap
+  [v2.10.0-M3+db1b777-bootstrap]=v2.10.0-M3-bootstrap
 )
 
 declare -A GROUP5_MODE=(
@@ -327,6 +329,7 @@ declare -A GROUP5_MODE=(
   [v2.10.0-M2+d4b8d8d-bootstrap]=build
   [v2.10.0-M2+fe1110f-bootstrap]=build
   [v2.10.0-M3-bootstrap]=build
+  [v2.10.0-M3+db1b777-bootstrap]=build
 )
 
 EXPECTED_ERROR2=(
--- deps/java8-legacy-stubs-src/java/util/Arrays.java
+++ deps/java8-legacy-stubs-src/java/util/Arrays.java
@@ -5,4 +5,14 @@ public final class Arrays {
   public static <T> java.util.List<T> asList(T... values) { return null; }
   public static void fill(int[] values, int value) {}
   public static <T> void sort(T[] values, Comparator<? super T> comparator) {}
+  public static <T> T[] copyOfRange(T[] original, int from, int to) { return null; }
+  public static <T, U> T[] copyOfRange(U[] original, int from, int to, Class<? extends T[]> newType) { return null; }
+  public static byte[] copyOfRange(byte[] original, int from, int to) { return null; }
+  public static short[] copyOfRange(short[] original, int from, int to) { return null; }
+  public static int[] copyOfRange(int[] original, int from, int to) { return null; }
+  public static long[] copyOfRange(long[] original, int from, int to) { return null; }
+  public static char[] copyOfRange(char[] original, int from, int to) { return null; }
+  public static float[] copyOfRange(float[] original, int from, int to) { return null; }
+  public static double[] copyOfRange(double[] original, int from, int to) { return null; }
+  public static boolean[] copyOfRange(boolean[] original, int from, int to) { return null; }
 }
