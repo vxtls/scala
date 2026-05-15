8e3e2a40ade199b2cbdb303a9b937392af08ba6a
vxtls <187420201+vxtls@users.noreply.github.com>
2026-05-15 01:23:12 -0400

    fix(build): add 2.10.1 bootstrap stage

--- buildit.sh
+++ buildit.sh
@@ -281,6 +281,7 @@ GROUP5=(
   v2.10.1-RC1-bootstrap
   v2.10.1-RC2-bootstrap
   v2.10.1-RC3-bootstrap
+  v2.10.1-bootstrap
 )
 
 declare -A GROUP4_PREV=(
@@ -434,12 +435,14 @@ declare -A GROUP5_PREV=(
   [v2.10.1-RC1-bootstrap]=v2.10.0-bootstrap
   [v2.10.1-RC2-bootstrap]=v2.10.1-RC1-bootstrap
   [v2.10.1-RC3-bootstrap]=v2.10.1-RC2-bootstrap
+  [v2.10.1-bootstrap]=v2.10.1-RC3-bootstrap
 )
 
 declare -A GROUP5_VERSION=(
   [v2.10.1-RC1-bootstrap]=2.10.1
   [v2.10.1-RC2-bootstrap]=2.10.1
   [v2.10.1-RC3-bootstrap]=2.10.1
+  [v2.10.1-bootstrap]=2.10.1
 )
 
 declare -A GROUP5_MODE=(
@@ -549,6 +552,7 @@ declare -A GROUP5_MODE=(
   [v2.10.1-RC1-bootstrap]=build
   [v2.10.1-RC2-bootstrap]=build
   [v2.10.1-RC3-bootstrap]=build
+  [v2.10.1-bootstrap]=build
 )
 
 EXPECTED_ERROR2=(
