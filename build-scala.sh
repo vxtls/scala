482ea16d8dd316d938f1cc968f2d0966f211924d
vxtls <187420201+vxtls@users.noreply.github.com>
2026-05-16 15:46:00 -0400

    fix(build): quote M1 partest debug bootclasspath

--- build-scala.sh
+++ build-scala.sh
@@ -133,7 +133,7 @@ java_home="${JAVA_HOME:?JAVA_HOME must point at a JDK 8 installation}"
 exec "$java_home/bin/java" \
   "-noverify" \
   "-Xbootclasspath/p:$script_dir/java8-partest-boot-stubs.jar" \
-  "-Dpartest.debug.settings=-javabootclasspath $script_dir/java8-charbuffer-overrides.jar:$script_dir/java8-legacy-stubs.jar:$script_dir/java8-filtered-stubs.jar:$java_home/jre/lib/rt.jar" \
+  "-Dpartest.debug.settings=-javabootclasspath \"$script_dir/java8-charbuffer-overrides.jar:$script_dir/java8-legacy-stubs.jar:$script_dir/java8-filtered-stubs.jar:$java_home/jre/lib/rt.jar\"" \
   "$@"
 EOF
   chmod +x "$partest_java_cmd"
@@ -147,7 +147,7 @@ java_bootclasspath="$script_dir/java8-charbuffer-overrides.jar:$script_dir/java8
 exec "$java_home/bin/java" \
   "-noverify" \
   "-Xbootclasspath/p:$script_dir/java8-buildmanager-boot-stubs.jar" \
-  "-Dpartest.debug.settings=-javabootclasspath $java_bootclasspath" \
+  "-Dpartest.debug.settings=-javabootclasspath \"$java_bootclasspath\"" \
   "$@"
 EOF
   chmod +x "$partest_icode_java_cmd"
