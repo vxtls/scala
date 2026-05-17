53fac43d56b494fc9e68ae352d3053381c543d41
vxtls <187420201+vxtls@users.noreply.github.com>
2026-05-17 08:42:19 -0400

    fix(build): build vendored MSIL dependency when referenced

--- build-scala.sh
+++ build-scala.sh
@@ -124,8 +124,7 @@ build_deps() {
   "$script_dir/deps/build-java8-charbuffer-overrides.sh" "$stage_dir" "$base_java8_stubs"
   "$script_dir/deps/build-java8-partest-boot-stubs.sh" "$stage_dir" "$java8_legacy_stubs_jar"
   if [[ -d "$stage_dir/src/msil" ]] \
-    && grep -q 'msil-source.jar' "$stage_dir/build.xml" \
-    && ! grep -q 'scala/tools/nsc/backend/MSILPlatform.scala' "$stage_dir/build.xml"; then
+    && rg -q 'ch\.epfl\.lamp\.compiler\.msil' "$stage_dir/src/compiler"; then
     "$script_dir/deps/build-msil-source.sh" "$stage_dir" "$starr_lib" "$starr_comp" "$java_bootclasspath" "$version_number"
   fi
   if needs_transition_bootstrap_compiler; then
