ba3b318df90d797fc311218592aac5e1273d700c
vxtls <187420201+vxtls@users.noreply.github.com>
2026-05-19 12:19:52 -0400

    fix(build): add 2.10.3 rc1 chain

--- buildit.sh
+++ buildit.sh
@@ -285,6 +285,7 @@ GROUP5=(
   v2.10.2-RC1-bootstrap
   v2.10.2-RC2-bootstrap
   v2.10.2-bootstrap
+  v2.10.3-RC1-bootstrap
 )
 
 declare -A GROUP4_PREV=(
@@ -442,6 +443,7 @@ declare -A GROUP5_PREV=(
   [v2.10.2-RC1-bootstrap]=v2.10.1-bootstrap
   [v2.10.2-RC2-bootstrap]=v2.10.2-RC1-bootstrap
   [v2.10.2-bootstrap]=v2.10.2-RC2-bootstrap
+  [v2.10.3-RC1-bootstrap]=v2.10.2-bootstrap
 )
 
 declare -A GROUP5_VERSION=(
@@ -452,6 +454,7 @@ declare -A GROUP5_VERSION=(
   [v2.10.2-RC1-bootstrap]=2.10.2
   [v2.10.2-RC2-bootstrap]=2.10.2
   [v2.10.2-bootstrap]=2.10.2
+  [v2.10.3-RC1-bootstrap]=2.10.3
 )
 
 declare -A GROUP5_MODE=(
@@ -564,6 +567,7 @@ declare -A GROUP5_MODE=(
   [v2.10.2-RC1-bootstrap]=build
   [v2.10.2-RC2-bootstrap]=build
   [v2.10.2-bootstrap]=build
+  [v2.10.3-RC1-bootstrap]=build
   [v2.10.1-bootstrap]=build
 )
 
