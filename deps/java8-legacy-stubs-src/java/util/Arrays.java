76b5ec05e42aee8c6192c7dcb03acb7a760e8477
vxtls <187420201+vxtls@users.noreply.github.com>
2026-05-20 10:05:26 -0400

    fix(java8): add arrays sort stub signatures

--- deps/java8-legacy-stubs-src/java/util/Arrays.java
+++ deps/java8-legacy-stubs-src/java/util/Arrays.java
@@ -4,7 +4,24 @@ public final class Arrays {
   private Arrays() {}
   public static <T> java.util.List<T> asList(T... values) { return null; }
   public static void fill(int[] values, int value) {}
+  public static void sort(byte[] values) {}
+  public static void sort(byte[] values, int fromIndex, int toIndex) {}
+  public static void sort(char[] values) {}
+  public static void sort(char[] values, int fromIndex, int toIndex) {}
+  public static void sort(double[] values) {}
+  public static void sort(double[] values, int fromIndex, int toIndex) {}
+  public static void sort(float[] values) {}
+  public static void sort(float[] values, int fromIndex, int toIndex) {}
+  public static void sort(int[] values) {}
+  public static void sort(int[] values, int fromIndex, int toIndex) {}
+  public static void sort(long[] values) {}
+  public static void sort(long[] values, int fromIndex, int toIndex) {}
+  public static void sort(short[] values) {}
+  public static void sort(short[] values, int fromIndex, int toIndex) {}
+  public static void sort(Object[] values) {}
+  public static void sort(Object[] values, int fromIndex, int toIndex) {}
   public static <T> void sort(T[] values, Comparator<? super T> comparator) {}
+  public static <T> void sort(T[] values, int fromIndex, int toIndex, Comparator<? super T> comparator) {}
   public static <T> T[] copyOfRange(T[] original, int from, int to) { return null; }
   public static <T, U> T[] copyOfRange(U[] original, int from, int to, Class<? extends T[]> newType) { return null; }
   public static byte[] copyOfRange(byte[] original, int from, int to) { return null; }
